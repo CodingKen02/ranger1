111d80cdbfc9af9f6bef3161a0aa02fc9c1b6e5f
CodingKen02 <71536021+CodingKen02@users.noreply.github.com>
2023-01-26 01:08:50 -0600

    Initial commit

---
+++ Ranger1.java
@@ -0,0 +1,23 @@
+/*
+Name: Kennedy Keyes
+Date: February 18, 2021
+Name of Program: Ranger1.java
+Description: This program will print out "Northwest CC Rangers" on a single 
+line. 
+ */
+package ranger1;
+
+/**
+ *
+ * @author codingken
+ */
+public class Ranger1 {
+// main method begins execution of Java application
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Northwest CC Rangers");
+    } // end method main
+    
+} // end class Ranger1
